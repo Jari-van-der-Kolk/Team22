0e1d9edc0b08d73124ae670f6f387ab6d6ba1ea9
Milanindra <65224551+Milanindra@users.noreply.github.com>
2023-04-24 16:40:45 +0200

    fixed small error

--- Team22/Assets/Game/Scripts/RaftBuilding/RaftBuilding.cs
+++ Team22/Assets/Game/Scripts/RaftBuilding/RaftBuilding.cs
@@ -5,34 +5,40 @@ using UnityEngine;
 
 public class RaftBuilding : MonoBehaviour
 {
+    [SerializeField] GameObject _floorPrefab;
+    
     private int[,] _raftGrid = new int[7, 4];
-
-    private Vector3 _raftOffset = new Vector3(8, 0, 6);
+    private Vector3 _raftOffset = new Vector3(6, 0, 4);
 
     private void Awake()
     {
-        raftGrid = new int[,]
+        _raftGrid = new int[,]
         {
-            {0,0,0,0,0,0,0},
-            {0,0,0,0,0,0,0},
-            {0,0,0,1,0,0,0},
-            {0,0,0,0,0,0,0},
-        }
+            { 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 1, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0 }
+        };
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        LoadRaft();
     }
 
-    private void UpdateRaft()
+    private void LoadRaft()
     {
-        for (int i = 0; i < raftGrid.GetLength(0); i++)
+        for (int i = 0; i < _raftGrid.GetLength(0); i++)
         {
-            for (int j = 0; j < raftGrid.GetLength(1); j++)
+            for (int j = 0; j < _raftGrid.GetLength(1); j++)
             {
-                
+                if (_raftGrid[i, j] == 1)
+                {
+                    var x = j - _raftOffset.x;
+                    var z = i - _raftOffset.z;
+                    var raft = Instantiate(_floorPrefab, new Vector3(x, 0, z) + _raftOffset, Quaternion.identity);
+                    raft.transform.parent = transform;
+                }
             }
         }
     }
@@ -40,12 +46,12 @@ public class RaftBuilding : MonoBehaviour
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
-        for (int i = 0; i < raftGrid.GetLength(0); i++)
+        for (int i = 0; i < _raftGrid.GetLength(1); i++)
         {
-            for (int j = 0; j < raftGrid.GetLength(1); j++)
+            for (int j = 0; j < _raftGrid.GetLength(0); j++)
             {
-                var x = i * 2 - raftGrid.GetLength(0) + 1;
-                var z = j * 2 - raftGrid.GetLength(1) + 2;
+                var x = i * 2 - _raftOffset.z;
+                var z = j * 2 - _raftOffset.x;
                 Gizmos.DrawWireCube(new Vector3(x, 0, z), new Vector3(2, .5f, 2));
             }
         }   
