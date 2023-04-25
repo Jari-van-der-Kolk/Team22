098da07754a348672fe99dc286c1d116b676a279
DR-Milan <210392@student.glu.nl>
2023-04-25 13:38:31 +0200

    Enemies roaming

--- Team22/Assets/Game/Scripts/Enemies/Enemy.cs
+++ Team22/Assets/Game/Scripts/Enemies/Enemy.cs
@@ -4,19 +4,26 @@ using UnityEngine;
 
 public enum States
 {
-    roaming, 
+    roaming,
     attacking
 }
 
 public class Enemy : MonoBehaviour
 {
-    [SerializeField] private GameObject _player;
+    [SerializeField] private Transform _player;
 
     [SerializeField] private States _states;
+    [SerializeField] private float _movementAmount;
+    [SerializeField] private float _movementSpeed = 5f; // adjust this to change the speed of movement
+    private Transform _transform;
+    private Vector3 _targetPosition;
 
-    private void Awake()
+    private float _distanceThreshold = 0.1f; // adjust this to change how close the enemy needs to get to the target position before choosing a new target
+
+    private void Start()
     {
-        _states = States.roaming;
+        _transform = transform;
+        ChooseNewRandomTargetPosition();
     }
 
     private void Update()
@@ -24,19 +31,28 @@ public class Enemy : MonoBehaviour
         switch (_states)
         {
             case States.roaming:
-                // Set a random direction and speed for the bird
-                Vector3 direction = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized;
-                float speed = Random.Range(1f, 5f);
 
-                // Rotate the bird in the direction it is moving
-                transform.rotation = Quaternion.LookRotation(direction);
+                _transform.position = Vector3.MoveTowards(_transform.position, _targetPosition, _movementSpeed * Time.deltaTime);
+                transform.LookAt(_targetPosition);
 
-                direction *= 10f;
-                // Move the bird in the chosen direction and speed
-                transform.position += direction * speed * Time.deltaTime;
+                if (Vector3.Distance(_transform.position, _targetPosition) < _distanceThreshold)
+                {
+                    ChooseNewRandomTargetPosition();
+                }
                 break;
+
             case States.attacking:
+
                 break;
         }
+    }
+
+    private void ChooseNewRandomTargetPosition()
+    {
+        float x = Random.Range(_player.position.x + (-10f * _movementAmount), _player.position.x + (10f * _movementAmount));
+        float y = Random.Range(_player.position.y + (-2f * _movementAmount), _player.position.y + (5f * _movementAmount));
+        float z = Random.Range(_player.position.z + (-10f * _movementAmount), _player.position.z + (10f * _movementAmount));
+
+        _targetPosition = new Vector3(x, y, z);
     }
 }
