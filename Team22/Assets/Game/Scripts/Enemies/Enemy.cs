2a5fca03d356e35d6c28ca55071bf7c7bf94b778
DR-Milan <210392@student.glu.nl>
2023-04-26 11:10:33 +0200

    sPAWNER

--- Team22/Assets/Game/Scripts/Enemies/Enemy.cs
+++ Team22/Assets/Game/Scripts/Enemies/Enemy.cs
@@ -10,7 +10,7 @@ public enum States
 
 public class Enemy : MonoBehaviour
 {
-    [SerializeField] private Transform _player;
+    public Transform _player;
 
     [SerializeField] private States _states;
     [SerializeField] private EnemyData _enemyData;
--- Team22/Assets/Game/Scripts/Enemies/SpawningEnemies.cs
+++ Team22/Assets/Game/Scripts/Enemies/SpawningEnemies.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class SpawningEnemies : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject _enemy;
+    [SerializeField] private float _spawnTime;
+    [SerializeField] private float _spawnDistance;
+    [SerializeField] private int _maxEnemiesToSpawn;
+    private Vector3 _spawnPosition;
+    private List<GameObject> _allEnemies = new List<GameObject>();
+
+    private void Start()
     {
-        
+        StartCoroutine(SpawnEnemies());
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator SpawnEnemies()
     {
-        
+
+        ChooseNewRandomSpawnPosition();
+        GameObject go = Instantiate(_enemy, _spawnPosition, Quaternion.identity);
+        _allEnemies.Add(go);
+        go.GetComponent<Enemy>()._player = transform;
+        yield return new WaitForSeconds(_spawnTime);
+        if (_allEnemies.Count <= _maxEnemiesToSpawn)
+            StartCoroutine(SpawnEnemies());
+
+    }
+    private void ChooseNewRandomSpawnPosition()
+    {
+        float x = Random.Range(transform.position.x + (-10f * _spawnDistance), transform.position.x + (10f * _spawnDistance));
+        float y = Random.Range(transform.position.y + (-10f * _spawnDistance), transform.position.y + (10f * _spawnDistance));
+        float z = Random.Range(transform.position.z + (-10f * _spawnDistance), transform.position.z + (10f * _spawnDistance));
+
+        _spawnPosition = new Vector3(x, y, z);
     }
 }
